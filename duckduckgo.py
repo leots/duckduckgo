a5e925dcf3c421d4fbdcd09e82655999108cf5f2
Leonidas Tsekouras <leotsekouras@gmail.com>
2018-02-12 16:55:12 +0200

    Retry search on duckduckgo error

--- duckduckgo.py
+++ duckduckgo.py
@@ -15,6 +15,9 @@ def search(keywords, max_results=None):
     while True:
         res = requests.post(url, data=params)
         doc = html.fromstring(res.text)
+        if 'If this error persists, please let us know' in doc.text_content():
+            time.sleep(20)
+            continue
 
         results = [(a.text_content(), a.get('href'))
                    for a in doc.cssselect('.result__a')]
